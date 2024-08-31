1fd90347d1eab366005de00cf89b3a85b826958f
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-08-30 23:07:05 -0500

    Update to only have one .readFile()

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 const http = require('http');
-const url = require('url')
 const fs = require('fs');
 
 http.createServer(function (req, res) {
   const q = req.url
-  const ex = url.parse(req.url, true)
+  let status = 200;
   let fileName = "";
-  console.log(ex)
 
   if (q === '/'){
     fileName = 'index.html'
-    fs.readFile(fileName, function (err, data) {
-        const ex = url.parse(req.url, true)
-        console.log(ex.pathname)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end();
-      });
   } else if (q === '/about'){
     fileName = 'about.html'
-    fs.readFile(fileName, function (err, data) {
-        const ex = url.parse(req.url, true)
-        console.log(ex.pathname)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end();
-      });
   } else if (q === '/contact-me'){
     fileName = 'contact-me.html'
-    fs.readFile(fileName, function (err, data) {
-        const ex = url.parse(req.url, true)
-        console.log(ex.pathname)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end();
-      });
   } else {
     fileName = '404.html'
-    fs.readFile(fileName, function (err, data) {
-        const ex = url.parse(req.url, true)
-        console.log(ex.pathname)
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end();
-    });
+    status = 404
   }
+  fs.readFile(fileName, function (err, data) {
+    res.writeHead(status, {'Content-Type': 'text/html'});
+    res.write(data)
+    return res.end();
+  });
 }).listen(8080);

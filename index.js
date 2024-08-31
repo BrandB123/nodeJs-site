6249843a3803bba846eb7d20ada0e1c78b43c7d6
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-08-30 22:57:14 -0500

    Add 404 status code

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const http = require('http');
+const url = require('url')
 const fs = require('fs');
 
 http.createServer(function (req, res) {
   const q = req.url
+  const ex = url.parse(req.url, true)
   let fileName = "";
+  console.log(ex)
 
   if (q === '/'){
     fileName = 'index.html'
+    fs.readFile(fileName, function (err, data) {
+        const ex = url.parse(req.url, true)
+        console.log(ex.pathname)
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end();
+      });
   } else if (q === '/about'){
     fileName = 'about.html'
+    fs.readFile(fileName, function (err, data) {
+        const ex = url.parse(req.url, true)
+        console.log(ex.pathname)
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end();
+      });
   } else if (q === '/contact-me'){
     fileName = 'contact-me.html'
+    fs.readFile(fileName, function (err, data) {
+        const ex = url.parse(req.url, true)
+        console.log(ex.pathname)
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end();
+      });
   } else {
     fileName = '404.html'
+    fs.readFile(fileName, function (err, data) {
+        const ex = url.parse(req.url, true)
+        console.log(ex.pathname)
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end();
+    });
   }
-  
-  fs.readFile(fileName, function (err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data)
-    return res.end();
-  });
 }).listen(8080);

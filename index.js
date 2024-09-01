630957e077ad79e544a17efd1ec436fbc20adb5a
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-08-31 22:11:10 -0500

    Update to use Express

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  const q = req.url
-  let status = 200;
-  let fileName = "";
+app.get('/', function (req, res) {
+  const fileName = 'index.html';
+  res.sendFile(fileName, {root: (__dirname)})
+  console.log('Routed to index.html')
+});
 
-  if (q === '/'){
-    fileName = 'index.html'
-  } else if (q === '/about'){
-    fileName = 'about.html'
-  } else if (q === '/contact-me'){
-    fileName = 'contact-me.html'
-  } else {
-    fileName = '404.html'
-    status = 404
-  }
-  fs.readFile(fileName, function (err, data) {
-    res.writeHead(status, {'Content-Type': 'text/html'});
-    res.write(data)
-    return res.end();
-  });
-}).listen(8080);
+app.get('/:file', function (req, res) {
+  const fileName = `${req.params.file}.html`;
+  res.sendFile(fileName, {root: (__dirname)}, function(err){
+    if (err){
+      res.sendFile('404.html', {root: (__dirname)});
+      console.log('Routed to 404.html')
+    } else {
+      console.log(`Routed to ${req.params.file}.html`)
+    }
+  })
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Express app is running - listening on ${PORT}`));
